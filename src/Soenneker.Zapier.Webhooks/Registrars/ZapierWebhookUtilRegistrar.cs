192930fcae3045f35a938b546c9ac1cd4ce30997
soenneker <jake@soenneker.com>
2026-08-29 14:17:28 -0500

    Improve public API XML documentation

--- src/Soenneker.Zapier.Webhooks/Registrars/ZapierWebhookUtilRegistrar.cs
+++ src/Soenneker.Zapier.Webhooks/Registrars/ZapierWebhookUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class ZapierWebhookUtilRegistrar
     /// <summary>
     /// Adds <see cref="IZapierWebhookUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddZapierWebhookUtilAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton().TryAddSingleton<IZapierWebhookUtil, ZapierWebhookUtil>();
@@ -23,6 +25,8 @@ public static class ZapierWebhookUtilRegistrar
     /// <summary>
     /// Adds <see cref="IZapierWebhookUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddZapierWebhookUtilAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton().TryAddScoped<IZapierWebhookUtil, ZapierWebhookUtil>();
